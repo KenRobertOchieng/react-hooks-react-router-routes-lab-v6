7e6824acf566681828cfe9386752183ca03a7052
Ken Robert Ochieng <kenrobertochi@gmail.com>
2025-04-25 18:20:27 +0300

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
-import "./index.css";
+// main.jsx
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import routeConfig from "./routes";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+const router = createBrowserRouter(routeConfig);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,28 @@
+// routes.jsx
+import Home from "./pages/Home";
+import Directors from "./pages/Directors";
+import Actors from "./pages/Actors";
+import Movie from "./pages/Movie";
+import ErrorPage from "./pages/ErrorPage";
 
+const routeConfig = [
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/directors",
+    element: <Directors />,
+  },
+  {
+    path: "/actors",
+    element: <Actors />,
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />,
+  },
+];
 
-const routes = [
-   
-  ];
-
-export default routes;
+export default routeConfig;
